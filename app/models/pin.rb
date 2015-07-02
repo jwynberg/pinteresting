c586633017bb792a36627226d87a21b7e8e010f4
Jacob Wynberg <jwynberg@gmail.com>
2015-07-02 13:08:02 +1000

    final s3 changes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,19 @@
+# class Pin < ActiveRecord::Base
+# 	belongs_to :user
+# 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+# 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+# end
+
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	has_attached_file :image, 
+            :styles => { :medium => "300x300>", :thumb => "100x100>" },
+            :storage => :s3,
+            :bucket  => ENV['S3_BUCKET_NAME']
+    validates :image, presence: true
+    validates :description, presence: true
+		validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about"
   get "home" => "pages#home"
+  get "pins" => "pages#pins"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
